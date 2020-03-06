f98ad0fba791e35d5d9da367b10b7a04ee423524
Goldpm <lowedavidadam@gmail.com>
2020-03-06 16:11:57 -0500

    added textbox

--- typicode.js
+++ typicode.js
@@ -7,8 +7,57 @@ const btn6 = $('#btn6')
 const btn7 = $('#btn7')
 const btn8 = $('#btn8')
 const btn9 = $('#btn9')
+const textbox = $('#textbox')
 
 btn1.click(function () {
+    $.get('https://jsonplaceholder.typicode.com/posts', function(response) {
+        console.log(response)
+    })
+});
+
+btn2.click(function () {
+    $.get('https://jsonplaceholder.typicode.com/posts', function(response) {
+        console.log(response)
+    })
+});
+
+btn3.click(function () {
+    $.get('https://jsonplaceholder.typicode.com/posts', function(response) {
+        console.log(response)
+    })
+});
+
+btn4.click(function () {
+    $.get('https://jsonplaceholder.typicode.com/posts', function(response) {
+        console.log(response)
+    })
+});
+
+btn5.click(function () {
+    $.get('https://jsonplaceholder.typicode.com/posts', function(response) {
+        console.log(response)
+    })
+});
+
+btn6.click(function () {
+    $.get('https://jsonplaceholder.typicode.com/posts', function(response) {
+        console.log(response)
+    })
+});
+
+btn7.click(function () {
+    $.get('https://jsonplaceholder.typicode.com/posts', function(response) {
+        console.log(response)
+    })
+});
+
+btn8.click(function () {
+    $.get('https://jsonplaceholder.typicode.com/posts', function(response) {
+        console.log(response)
+    })
+});
+
+btn9.click(function () {
     $.get('https://jsonplaceholder.typicode.com/posts', function(response) {
         console.log(response)
     })
