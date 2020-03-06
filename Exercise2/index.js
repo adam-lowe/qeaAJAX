089ace2df8920035385dc4a1042ed2c6389736f8
Goldpm <lowedavidadam@gmail.com>
2020-03-06 18:36:09 -0500

    made fetch and matcher for usernames

--- Exercise2/index.js
+++ Exercise2/index.js
@@ -4,6 +4,18 @@ const submit = $("#submit")
 
 submit.click(function (e) { 
     e.preventDefault();
-    
-    
-});
+    fetch('http://jsonplaceholder.typicode.com/users')
+    .then(function(response) {
+            return response.json();
+          })
+          .then((data) => {
+            console.log(data);
+            console.log(userField.val());
+            for (var i=0; i < data.length; i++) {
+                if (data[i].username === userField.val()) {
+                    console.log("Match!")
+                    return data[i];
+                }
+            }
+          });
+    });
