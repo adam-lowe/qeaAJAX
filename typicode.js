89b133ac731fcf551361a1108104c2adfc6908da
Goldpm <lowedavidadam@gmail.com>
2020-03-06 16:07:58 -0500

    added jquery buttons

--- typicode.js
+++ typicode.js
@@ -1,3 +1,15 @@
-$.get('https://jsonplaceholder.typicode.com/posts', function(response) {
-    console.log(response)
-})
+const btn1 = $('#btn1')
+const btn2 = $('#btn2')
+const btn3 = $('#btn3')
+const btn4 = $('#btn4')
+const btn5 = $('#btn5')
+const btn6 = $('#btn6')
+const btn7 = $('#btn7')
+const btn8 = $('#btn8')
+const btn9 = $('#btn9')
+
+btn1.click(function () {
+    $.get('https://jsonplaceholder.typicode.com/posts', function(response) {
+        console.log(response)
+    })
+});
