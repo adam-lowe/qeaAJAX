49e64d9b6b48a09b5379c276c873d36ad6fe531a
Goldpm <lowedavidadam@gmail.com>
2020-03-07 17:53:45 -0500

    implemented localstorage into verification

--- Exercise2/index.js
+++ Exercise2/index.js
@@ -14,6 +14,7 @@ submit.click(function (e) {
             for (var i=0; i < data.length; i++) {
                 if (data[i].username === userField.val()) {
                     console.log("Match!")
+                    localStorage.setItem("user", userField.val());
                     window.location = 'homepage.html'
                     return data[i];
                 }
