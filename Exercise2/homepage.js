fdb21c56ce2a7d424beb9dda27003a04b34eda26
Goldpm <lowedavidadam@gmail.com>
2020-03-07 17:57:38 -0500

    added custom welcome

--- Exercise2/homepage.js
+++ Exercise2/homepage.js
@@ -1,9 +1,11 @@
 $(document).ready(function(){
+    let currentUser
     if (localStorage.getItem("user")) {
-       let currentUser = localStorage.getItem("user")
+       currentUser = localStorage.getItem("user")
         localStorage.removeItem("user");
     }
     else {
         window.location = 'index.html'
     }
+    $("span").text(currentUser)
   });
