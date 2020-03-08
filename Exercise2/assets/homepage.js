64de9bf0467439d4ad8b754f210c7eeb227ba476
Goldpm <lowedavidadam@gmail.com>
2020-03-07 23:35:38 -0500

    formatted

--- Exercise2/assets/homepage.js
+++ Exercise2/assets/homepage.js
@@ -8,7 +8,15 @@ $(document).ready(function() {
     window.location = "index.html";
   }
   $(".currentUser").text(currentUser);
-
+  $("#logOut").click(function() {
+    localStorage.removeItem("user");
+    localStorage.removeItem("userId");
+    location.reload();
+  });
+  $("#photoSearch").change(function(e) {
+    e.preventDefault();
+    $("#aBody").filter($("#photoSearch").val());
+  });
   fetch(`https://jsonplaceholder.typicode.com/users/${currentUserId}/posts`)
     .then(response => {
       return response.json();
@@ -31,74 +39,72 @@ $(document).ready(function() {
         `);
       });
     });
-    $("#logOut").click(function() {
-      localStorage.removeItem("user");
-      localStorage.removeItem("userId");
-      location.reload();
-    });
-    $("#photoSearch").change(function (e) { 
-      e.preventDefault();
-      
-    });
-    
-    $("#posts").click(function (event) {
-        if (event.target.id !== "posts") {
-            let postIndex = (event.target.id).replace('post','');
-            
-             fetch(`https://jsonplaceholder.typicode.com/users/${currentUserId}/posts`)
-              .then((response) => {
-                return response.json();
-              })
-              .then((data) => {
-                let post = data[postIndex]
-                $("#pTitle").text(post.title)
-                $("#pBody").text(post.body)
-                console.log(post);
-                $("#postModal").modal()
-                return fetch(`https://jsonplaceholder.typicode.com/posts/${post.id}/comments`);
-              })
-              .then((response) => {
-                return response.json();
-              })
-              .then((data) => {
-                data.forEach(function(item, index) {
-                  $("#pComments").append(`
+
+  $("#posts").click(function(event) {
+    if (event.target.id !== "posts") {
+      let postIndex = event.target.id.replace("post", "");
+
+      fetch(`https://jsonplaceholder.typicode.com/users/${currentUserId}/posts`)
+        .then(response => {
+          return response.json();
+        })
+        .then(data => {
+          let post = data[postIndex];
+          $("#pTitle").text(post.title);
+          $("#pBody").text(post.body);
+          console.log(post);
+          $("#postModal").modal();
+          return fetch(
+            `https://jsonplaceholder.typicode.com/posts/${post.id}/comments`
+          );
+        })
+        .then(response => {
+          return response.json();
+        })
+        .then(data => {
+          data.forEach(function(item, index) {
+            $("#pComments").append(`
                      <li id="comment${index}">${item.body}</li>
                   `);
-                });
-                console.log(data)
-              })
-        }
-    });
-    $("#albums").click(function (event) {
-      if (event.target.id !== "album") {
-        let albumTitle
-        let albumIndex = (event.target.id).replace('album','');
-        fetch(`https://jsonplaceholder.typicode.com/users/${currentUserId}/albums`)
-        .then((response) => {
+          });
+          console.log(data);
+        });
+    }
+  });
+  $("#albums").click(function(event) {
+    if (event.target.id !== "album") {
+      let albumTitle;
+      let albumIndex = event.target.id.replace("album", "");
+      fetch(
+        `https://jsonplaceholder.typicode.com/users/${currentUserId}/albums`
+      )
+        .then(response => {
           return response.json();
         })
-        .then((data) => {
-          let album = data[albumIndex]
-          $("#aTitle").text(album.title)
-          $('#albumModal').modal();
+        .then(data => {
+          let album = data[albumIndex];
+          $("#aTitle").text(album.title);
+          $("#albumModal").modal();
           console.log(album);
-          return fetch(`https://jsonplaceholder.typicode.com/albums/${album.id}/photos`);
+          return fetch(
+            `https://jsonplaceholder.typicode.com/albums/${album.id}/photos`
+          );
         })
-        .then((response) => {
+        .then(response => {
           return response.json();
         })
-        .then((data) => {
-          $("#aBody").empty()
+        .then(data => {
+          $("#aBody").empty();
           data.forEach(function(item, index) {
             $("#aBody").append(`
+              <div class="${item.title}">
               <p>${item.title}</p>
               <img src="${item.thumbnailUrl}">
+              </div>
             `);
           });
           console.log(data);
         });
-      }
-    });
+    }
+  });
 });
-
