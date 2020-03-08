89c7cf53521b68beffba9d898fba2d6793590f89
Goldpm <lowedavidadam@gmail.com>
2020-03-07 19:51:14 -0500

    added posts and albums

--- Exercise2/homepage.js
+++ Exercise2/homepage.js
@@ -1,18 +1,40 @@
-$(document).ready(function(){
-    let currentUser
-    let currentUserId
-    if (localStorage.getItem("user")) {
-       currentUser = localStorage.getItem("user")
-       currentUserId = localStorage.getItem("userId")
-    }
-    else {
-        window.location = 'index.html'
-    }
-    $("span").text(currentUser)
-  });
+$(document).ready(function() {
+  let currentUser;
+  let currentUserId;
+  if (localStorage.getItem("user")) {
+    currentUser = localStorage.getItem("user");
+    currentUserId = localStorage.getItem("userId");
+  } else {
+    window.location = "index.html";
+  }
+  $("span").text(currentUser);
 
-  $("#logOut").click(function () { 
-    localStorage.removeItem("user");
-    localStorage.removeItem("userId");
-    location.reload();
-  });
+  fetch(`https://jsonplaceholder.typicode.com/users/${currentUserId}/posts`)
+    .then(response => {
+      return response.json();
+    })
+    .then(data => {
+      data.forEach(function(item, index) {
+        $("#posts").append(`
+           <li id="post${index+1}">${item.title}</li>
+        `);
+      });
+    });
+  fetch(`https://jsonplaceholder.typicode.com/users/${currentUserId}/albums`)
+    .then(response => {
+      return response.json();
+    })
+    .then(data => {
+      data.forEach(function(item, index) {
+        $("#albums").append(`
+           <li id="album${index+1}">${item.title}</li>
+        `);
+      });
+    });
+});
+
+$("#logOut").click(function() {
+  localStorage.removeItem("user");
+  localStorage.removeItem("userId");
+  location.reload();
+});
